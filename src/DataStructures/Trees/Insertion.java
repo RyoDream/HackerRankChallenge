a2ac73f450b4f3431da3d9833bc92e6d02cb2e8b
Ryo <shiyanch@gmail.com>
2016-05-14 08:51:17 +0800

    Data Structures/Trees/Insertion

---
+++ src/DataStructures/Trees/Insertion.java
@@ -0,0 +1,67 @@
+package DataStructures.Trees;
+
+/**
+ * Binary Search Tree : Insertion
+ *
+ * You are given a pointer to the root of a binary search tree and a value to be inserted into the tree.
+ * Insert this value into its appropriate position in the binary search tree
+ * and return the root of the updated binary tree.
+ * You just have to complete the function.
+ *
+ * Input Format
+ * You are given a function,
+ *
+ * node * insert (node * root ,int value)
+ * {
+ *
+ * }
+ *
+ * node is defined as :
+ * struct node
+ * {
+ *  int data;
+ *  node * left;
+ *  node * right;
+ *  }node;
+ *
+ *  Output Format
+ *  Return the root of the binary search tree after inserting the value into the tree.
+ *
+ *  Sample Input
+ *      4
+ *     / \
+ *    2   7
+ *   / \
+ *  1   3
+ *
+ *  The value to be inserted is 6.
+ *
+ *  Sample Output
+ *       4
+ *     /   \
+ *    2     7
+ *   / \   /
+ *  1   3 6
+ */
+public class Insertion {
+    class Node {
+        int data;
+        Node left;
+        Node right;
+    }
+
+    Node Insert(Node root, int value) {
+        if(root == null) {
+            Node newNode = new Node();
+            newNode.data = value;
+            return newNode;
+        }
+
+        if(root.data < value)
+            root.right = Insert(root.right, value);
+        else
+            root.left = Insert(root.left, value);
+
+        return root;
+    }
+}
